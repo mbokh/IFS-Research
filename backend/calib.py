Ignore occluded particles for spectra and fixed negative overflow bug

backend/calib.py
@@ -5,6 +5,7 @@ from scipy.interpolate import interp1d
 
 #-------------------------------------------------------------------------------------------
 #Looking at the AL3Zr_SM_TrippleNotch for wavelength and pixel space positions
+FLIP_SOURCES_LR = True
 ##### REVERSE IMAGE LEFT TO RIGHT TO HAVE WAVELENGTH INCREASING WITH X
 ## IMAGE WIDTH IS 896 SO:
 
@@ -13,15 +14,19 @@ from scipy.interpolate import interp1d
 #532 nm peak: [567, 280]  ->  328
 #450 nm peak: [652, 278]  ->  243
 
-pixelPositions = [243, 328, 439] #X axis data
-wavelengths = [450, 532, 633] #Y axis data
-samples = np.interp([300, 400], pixelPositions, wavelengths)
-PIXEL_TO_WAVELENGTH_SCALING = (samples[1] - samples[0]) / 100
-offsetToPeak = (328 - 866, 280 - 281) #532 nm peak
-SPECTRA_ANGLE = 0.6 #math.degrees(math.atan2(278 - 281, 652 - 456)) According to full spectra image, it is 0.5
+pixelPositions = [456, 567, 652] #X axis data
+if FLIP_SOURCES_LR:
+	pixelPositions = [895 - i for i in pixelPositions]
 
-#Flip image left to right
-fullSpectra = np.fliplr(cv2.imread("calibrationData/Cal_Thorlabs_LampScan_30umSlit000001.tif")[450:600, :]) #Horizontal strip
+wavelengths = [450, 532, 633] #Y axis data
+samples = np.interp([pixelPositions[0], pixelPositions[2]], pixelPositions, wavelengths)
+PIXEL_TO_WAVELENGTH_SCALING = (samples[1] - samples[0]) / (pixelPositions[2] - pixelPositions[0])
+SPECTRA_ANGLE = 0.6 * (1 if FLIP_SOURCES_LR else -1) #math.degrees(math.atan2(278 - 281, 652 - 456)) According to full spectra image, it is 0.5
+
+
+fullSpectra = cv2.imread("calibrationData/Cal_Thorlabs_LampScan_30umSlit000001.tif")[450:600, :] #Horizontal strip
+if FLIP_SOURCES_LR: #Flip image left to right
+	fullSpectra = np.fliplr(fullSpectra)
 rotate_matrix = cv2.getRotationMatrix2D(center=(512, 75), angle=SPECTRA_ANGLE, scale=1)
 height, width = fullSpectra.shape[:2]
 gray = cv2.cvtColor(cv2.warpAffine(src=fullSpectra, M=rotate_matrix, dsize=(width, height)), cv2.COLOR_BGR2GRAY)
@@ -31,11 +36,12 @@ subsection = gray[30:60, :] #Take a chunk of image as the background
 backgroundValue = int(np.sum(subsection, axis=None) / (30 * 1024)) #Average value
 gray = gray - backgroundValue
 
-binarySpectra = cv2.threshold(gray[:, :823], 2, 255, cv2.THRESH_BINARY)[1]
-binaryParticle = cv2.threshold(gray[:, 823:], 15, 255, cv2.THRESH_BINARY)[1]
+boundary = 800 if FLIP_SOURCES_LR else 200
+binarySpectra = cv2.threshold(gray[:, :boundary], 2, 255, cv2.THRESH_BINARY)[1]
+binaryParticle = cv2.threshold(gray[:, boundary:], 15, 255, cv2.THRESH_BINARY)[1]
 combinedBinary = gray.copy()
-combinedBinary[:, :823] = binarySpectra
-combinedBinary[:, 823:] = binaryParticle
+combinedBinary[:, :boundary] = binarySpectra
+combinedBinary[:, boundary:] = binaryParticle
 
 numRegions, labels, stats, centroids = cv2.connectedComponentsWithStats(combinedBinary, 8, cv2.CV_32S)
 
@@ -60,9 +66,8 @@ particleData = data[1 - spectraIndex]
 def getWavelengthFromPixelPosition(x):
 	return int(PIXEL_TO_WAVELENGTH_SCALING * (x - pixelPositions[1]) + wavelengths[1])
 
-#Need to account for offset between slit and full image
-MAX_WAVELENGTH = min(800, getWavelengthFromPixelPosition(spectraData[0] + spectraData[2] + (866 - particleData[4]))) / 1000000000
-MIN_WAVELENGTH = max(550, getWavelengthFromPixelPosition(spectraData[0] + (866 - particleData[4]))) / 1000000000
+MAX_WAVELENGTH = 800 / 1000000000
+MIN_WAVELENGTH = 550 / 1000000000
 
 def getPixelPositionFromWavelength(w):
 	return int(((w - wavelengths[1]) / PIXEL_TO_WAVELENGTH_SCALING) + pixelPositions[1])

backend/particle.py
@@ -42,8 +42,5 @@ class Particle:
 		self.spectraData.append((data[0], data[1].astype(np.uint64).tolist(), data[2]))  #Want spectra as simple python list so that it can be pickled
 		assert(len(self.spectraData) == len(self.particleData)) #Also values are ~10^11+, round to int to save space in pickle and csv file
 
-	def prepareForPickling(self, videoWidth):
+	def prepareForPickling(self):
 		self.kalmanFilter = None #Can't pickle kalman, and we don't need it anyway, so simply destroy it
-		for d in self.particleData:
-			d[0][0] = (videoWidth - 1) - (d[0][0] + d[0][2] - 1) #Watch flip ordering
-			d[0][4] = (videoWidth - 1) - (d[0][4])

backend/spectraExtract.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 import math
+import copy
 import matplotlib.pyplot as plt
 
 from backend import calib as calibration, colorID, conflictResolution as conflictResolution, conversion as conversion, database
@@ -69,8 +70,12 @@ def extractRawSpectra(frame, video):
 	cv2.line(padded, (origWidth - 1, padding), (origWidth - 1, origHeight - 1 + padding), (255, 255, 255), 1)
 	debugImage = cv2.warpAffine(src=padded, M=rotate_matrix, dsize=(width, height))
 
+	finalSpectra = dict()  # pId -> temp, spectra, code
 	particles = dict() #Contain pId -> ((left, top, right, bottom), particleWidth)
 	for particleId, box, occlusionCount in database.getLastBoundingBoxes():
+		if occlusionCount > 0:
+			finalSpectra[particleId] = (0, np.zeros(calibration.SYSTEM_RESPONSE.size), 5)
+			continue
 		x1, y1, w1, h1, cX1, cY1 = box
 		mappedCX, mappedCY = mapParticleToSpectra(cX1, cY1)
 		#Length preserved
@@ -92,7 +97,7 @@ def extractRawSpectra(frame, video):
 		if code == 0 or code == 1:
 			resolvableConflicts[i] = (averageOverRanges(gray, particles[i][0][0], particles[i][0][2], data, particles[i][1]), code)
 		else:
-			unresolvableConflicts[i] = (data, gray.copy()) # A little expensive to copy, but worth the trouble
+			unresolvableConflicts[i] = (data, copy.deepcopy(gray).astype(np.int16)) # A little expensive to copy, but worth the trouble, make sure allow for negative values to prevent overflow
 
 
 	#Try to subtract away known spectra
@@ -103,7 +108,7 @@ def extractRawSpectra(frame, video):
 			for c in list(conflicts): #Remove conflicts when spectra are known
 				if c in resolvableConflicts:
 					xRange, yRange, spectraSegment = getSubtractionBounds(particles[conflictId][0], particles[c][0], resolvableConflicts[c][0]) #Subtraction is broadcasted to each row
-					imageSlice[yRange[0]:(yRange[1] + 1), xRange[0]:(xRange[1] + 1)] = imageSlice[yRange[0]:(yRange[1] + 1), xRange[0]:(xRange[1] + 1)] - spectraSegment #Can't use -= with numpy slice
+					imageSlice[yRange[0]:(yRange[1] + 1), xRange[0]:(xRange[1] + 1)] = np.clip(imageSlice[yRange[0]:(yRange[1] + 1), xRange[0]:(xRange[1] + 1)] - spectraSegment, a_min=0, a_max=None)  #Can't use -= with numpy slice
 					conflicts.remove(c)
 			data, code = getConflictType(conflictId, particles, conflicts)
 			if code == 0 or code == 1:
@@ -114,7 +119,7 @@ def extractRawSpectra(frame, video):
 			break
 		oldLength = len(unresolvableConflicts)
 
-	finalSpectra = dict() #pId -> temp, spectra, code
+
 	#Now, have to do least-squares or brute for demixing
 	while len(unresolvableConflicts) > 0:
 		conflictId = list(unresolvableConflicts.keys())[0]
@@ -134,6 +139,9 @@ def extractRawSpectra(frame, video):
 			finalSpectra[pId] = (resultDict[pId][0], resultDict[pId][1], 4)
 			if conflictId in unresolvableConflicts:
 				del unresolvableConflicts[pId]
+		#finalSpectra[conflictId] = (resultDict[conflictId][0], resultDict[conflictId][1], 4)
+		#del unresolvableConflicts[conflictId]
+
 
 	#Add from data from resolvable conflicts
 	for pId in resolvableConflicts:
@@ -151,7 +159,7 @@ def extractRawSpectra(frame, video):
 
 	#Log in database
 	database.addSpectraData(finalSpectra)
-	return debugImage
+	return debugImage, finalSpectra
 
 
 def getSubtractionBounds(baseBox, conflictBox, conflictSpectra): #Particle positions

mainCreateCompiledVideo.py
@@ -2,7 +2,7 @@ import cv2
 import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticks
-from backend import colorID
+from backend import colorID, calib
 
 import sources.VideoSource as VideoSource
 import pickle
@@ -190,7 +190,7 @@ def colorRegionWithTemperature(img, T, bBox):
 	img[bBox[1]:bBox[1] + bBox[3] + 1, bBox[0]:bBox[0] + bBox[2] + 1] = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
 
 def coordTransform(coords):
-	return (coords[0]) * 4, (coords[1] - 220) * 4, coords[2] * 4, coords[3] * 4, (coords[4]) * 4, (coords[5] - 220) * 4
+	return (coords[0] - 720) * 4, (coords[1] - 220) * 4, coords[2] * 4, coords[3] * 4, (coords[4] - 720) * 4, (coords[5] - 220) * 4
 
 
 matplotlib.use('TkAgg')
@@ -201,12 +201,12 @@ plt.ion()
 plt.show()
 
 
-outVideo = cv2.VideoWriter("extractedData/CompiledVideoRestricted.mp4", cv2.VideoWriter_fourcc(*'DIVX'), 10, (2000, 1200))
+outVideo = cv2.VideoWriter("extractedData/CompiledVideo.mp4", cv2.VideoWriter_fourcc(*'DIVX'), 10, (2000, 1200))
 
-with open('extractedData/extractedDataRestrictedRange.pickle', 'rb') as f:
+with open('extractedData/extractedData.pickle', 'rb') as f:
 	minWavelength, maxWavelength, particles = pickle.load(f)
 
-	video = VideoSource.VideoSource(filename="Al3Zr_SM_30k_Run2.avi", skip=0, end=-1, spectraStart=150, spectraEnd=1024 - 1, flipLR=False)
+	video = VideoSource.VideoSource(filename="Al3Zr_SM_30k_Run2.avi", skip=0, end=-1, spectraStart=150, spectraEnd=1024 - 1, flipLR=calib.FLIP_SOURCES_LR)
 
 	while True:
 		frame, frameNum = video.getFrame()
@@ -214,27 +214,29 @@ with open('extractedData/extractedDataRestrictedRange.pickle', 'rb') as f:
 			print("Video Done")
 			break
 
-		resized = cv2.resize(frame[220:520, 0:500], (2000, 1200)) #X, Y
-		resized = decorateFrame(particles, resized, frameNum, showDebugInfo=True, showPath=False, fullBoundingBox=False)
+		outFrame = np.zeros((1200, 2000, 3), dtype=np.uint8)
+		outFrame[:1200, :1200] = cv2.resize(frame[220:520, 720:1020], (1200, 1200))  # X, Y
+		#resized = cv2.resize(frame[220:520, 0:500], (2000, 1200)) #X, Y
+		resized = decorateFrame(particles, outFrame, frameNum, showDebugInfo=True, showPath=False, fullBoundingBox=False)
 
 		makeTempGraph(particles, frameNum)
 
 		img = np.array(graph.canvas.buffer_rgba())
 		img = cv2.cvtColor(img, cv2.COLOR_RGBA2BGR)
 
-		resized[:img.shape[0], -img.shape[1]:] = img
+		outFrame[:img.shape[0], -img.shape[1]:] = img
 
 		makeSpectraGraph(particles, frameNum, minWavelength, maxWavelength)
 
 		img = np.array(graph.canvas.buffer_rgba())
 		img = cv2.cvtColor(img, cv2.COLOR_RGBA2BGR)
 
-		resized[-img.shape[0]:, -img.shape[1]:] = img
+		outFrame[-img.shape[0]:, -img.shape[1]:] = img
 
 
 		if frameNum % 50 == 0:
 			print(frameNum)
-		outVideo.write(resized)
+		outVideo.write(outFrame)
 
 		#cv2.imshow('Data', resized)
 		#if cv2.waitKey(0) & 0xFF == ord('q'):

mainCreateSpectraVideo.py
@@ -58,7 +58,7 @@ outVideo = None
 plt.ion()
 plt.show()
 
-with open('extractedData/extractedDataRestrictedRange.pickle', 'rb') as f:
+with open('extractedData/extractedData.pickle', 'rb') as f:
 	minWavelength, maxWavelength, particleData = pickle.load(f)
 	frameNum = 0
 
@@ -118,10 +118,13 @@ with open('extractedData/extractedDataRestrictedRange.pickle', 'rb') as f:
 
 		if outVideo is None:
 			s = fig.get_size_inches()*fig.dpi
-			outVideo = cv2.VideoWriter("extractedData/spectraVideoRestricted.mp4", cv2.VideoWriter_fourcc(*'DIVX'), 10, (int(s[0]), int(s[1])))
+			outVideo = cv2.VideoWriter("extractedData/spectraVideo.mp4", cv2.VideoWriter_fourcc(*'DIVX'), 10, (int(s[0]), int(s[1])))
 		img = np.array(fig.canvas.buffer_rgba())
 		img = cv2.cvtColor(img, cv2.COLOR_RGBA2BGR)
 		outVideo.write(img)
+
+		if frameNum % 50 == 0:
+			print(frameNum)
 
 		plt.pause(0.01)
 		frameNum += 1